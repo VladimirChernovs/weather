559327ca5027e39d18249966ec9a8e48d2b0ce75
VladimirChernov <vladimirsjob@gmail.com>
2018-12-02 02:58:06 +0300

    Kotlin start

--- src/main/java/com/city/weather/utl/exeptions/CityExistException.java
+++
@@ -1,7 +0,0 @@
-package com.city.weather.utl.exeptions;
-
-public class CityExistException extends RuntimeException {
-    public CityExistException() {
-        super("City already exist!");
-    }
-}
