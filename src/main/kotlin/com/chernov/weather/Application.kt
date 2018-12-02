5c6829095245dc216bed4df25a74df39875a7d41
VladimirChernov <vladimirsjob@gmail.com>
2018-12-02 03:48:28 +0300

    refactoring

--- src/main/kotlin/com/chernov/weather/Application.kt
+++ src/main/kotlin/com/chernov/weather/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class WeatherApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<WeatherApplication>(*args)
+    runApplication<Application>(*args)
 }
