b14cbf4d47c4979984f8f0f41334850068ec85ca
VladimirChernovs <leninsbox@gmail.com>
2018-12-05 19:01:03 +0300

    + OpenWeatherApi

---
+++ src/main/kotlin/com/chernov/weather/web/api/OpenWeatherApi.kt
@@ -0,0 +1,64 @@
+package com.chernov.weather.web.api
+
+import com.chernov.weather.domain.dto.CityWeatherDTO
+import com.chernov.weather.domain.dto.WeatherDTO
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.ClientResponse
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+/**
+ * API for weather site
+ */
+@Component
+class OpenWeatherApi(private val properties: SiteProperties, private val webClientApi: WebClientApi) {
+
+    fun inCity(city: String, media: String): Mono<ServerResponse> {
+
+        return getUri(city, media)
+                .exchange()
+                .flatMap { mapper: ClientResponse ->
+                    ServerResponse.status(mapper.statusCode())
+                            .headers { c -> mapper.headers().asHttpHeaders()
+                                    .forEach { key, value -> c.put(key, value) }
+                            }
+                            .body(mapper.bodyToMono(String::class.java), String::class.java)
+                }
+    }
+
+    private fun getUri(city: String, media: String): WebClient.RequestHeadersSpec<*> {
+        return webClientApi!!.createWebClient()
+                .get()
+                .uri { builder ->
+                    builder.scheme("http")
+                            .host(properties.host).path(properties.path!!)
+                            .queryParam("q", this.urlParamDecoder(city))
+                            .queryParam("appid", properties.appid)
+                            .queryParam("mode", media)
+                            .build()
+                }
+    }
+
+//    @SneakyThrows
+    private fun urlParamDecoder(city: String): String {
+        return URLEncoder.encode(city, StandardCharsets.UTF_8.toString())
+    }
+
+    fun inCityMedias(city: String): Mono<CityWeatherDTO> {
+
+        val retrieve = getUri(city, "json").retrieve()
+        val dto = retrieve.bodyToMono(WeatherDTO::class.java)
+        val json = retrieve.bodyToMono(String::class.java)
+
+        val xml = getUri(city, "xml").retrieve().bodyToMono(String::class.java)
+
+        val zip = Mono.zip(dto, json, xml)
+        return zip.map { n ->
+            val cityId = java.lang.Long.parseLong(n.t1.id)
+            CityWeatherDTO(cityId, n.t2, n.t3)
+        }
+    }
+}
