b40ebfa21b3f31708edb5cfd49d0353f6735a724
VladimirChernov <vladimirsjob@gmail.com>
2018-12-10 00:09:43 +0300

    find one city handler refactoring

--- src/main/kotlin/com/chernov/weather/web/cities/CityHandler.kt
+++ src/main/kotlin/com/chernov/weather/web/cities/CityHandler.kt
@@ -1,20 +1,25 @@
 package com.chernov.weather.web.cities
 
 import com.chernov.weather.domain.dto.CityDTO
+import com.chernov.weather.domain.entities.City
+import com.chernov.weather.domain.entities.Weather
 import com.chernov.weather.services.CityService
+import com.chernov.weather.web.api.OpenWeatherApi
 import com.chernov.weather.web.common.validate
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.body
 import reactor.core.publisher.Mono
+import java.time.LocalDateTime
 
 /**
  *  Handle requests from router
  */
 @Component
-class CityHandler(val cityService: CityService) {
+class CityHandler(private val openWeatherApi: OpenWeatherApi, private val cityService: CityService) {
     fun findAll(req: ServerRequest): Mono<ServerResponse> = validate
             .request(req) {
                 ok().body(cityService.findAll())
@@ -22,13 +27,26 @@ class CityHandler(val cityService: CityService) {
 
     fun findOne(req: ServerRequest): Mono<ServerResponse> = validate
             .request(req) {
-                ok().body(cityService.findOne(getTheNameParameter(req)))
+                val cityName = getTheNameParameter(req)
+                val cityBody = cityService.findOne(cityName)
+                val mediaType = getMediaType(req)
+                cityBody.flatMap {
+                    ServerResponse.ok()
+                            .contentType(mediaType)
+                            .header("db-update-time", it.updateTime.toString())
+                            .body(cityBody.map { city ->
+                                when (mediaType) {
+                                    MediaType.APPLICATION_XML -> city.weather.xml
+                                    else -> city.weather.json
+                                }
+                            })
+                }.switchIfEmpty(openWeatherApi.inCity(cityName, mediaType.subtype))
             }
 
     fun create(req: ServerRequest) = validate
             .request(req)
             .withBody(CityDTO::class.java) { city ->
-                ok().body(cityService.create(city.name))
+                ok().body(cityService.create(City(3, 3, city.name, Weather("json", "xml"), LocalDateTime.now())))
             }
 
     fun deleteOne(req: ServerRequest): Mono<ServerResponse> = validate
@@ -37,4 +55,20 @@ class CityHandler(val cityService: CityService) {
             }
 
     private fun getTheNameParameter(req: ServerRequest) = req.queryParam("name").get()
+
+    private fun getMediaType(req: ServerRequest): MediaType {
+        val mediaType = req.headers().contentType()
+        if (mediaType.isPresent) {
+            val type = mediaType.get()
+            when (type) {
+                MediaType.APPLICATION_XML -> return type
+            }
+        }
+        val mediaMode = req.queryParam("mode")
+        if (mediaMode.isPresent) when (mediaMode.get()) {
+            "xml" -> return MediaType.APPLICATION_XML
+        }
+        return MediaType.APPLICATION_JSON
+    }
+
 }
